fe6ab0b286975ad019a3452070d138582a84872f
gondar-software <monate615@gmail.com>
2025-07-19 14:00:55 +0000

    add useAssessors hook

---
+++ frontend/src/hooks/useAssessors.ts
@@ -0,0 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
+import { Assessor } from "@/types/packets";
+import { apiRequest } from "@/lib/queryClient";
+
+const AUTH_KEY = 'assessors';
+
+async function fetchAssessors() {
+  const response = await apiRequest("GET", "/api/assessors/", {
+    useToken: true
+  });
+  if (response.status === 200) {
+    const assessorsData = await response.json();
+    return {
+      ...assessorsData,
+    }
+  }
+  else return null;
+}
+
+export const useAssessors = () => {
+  const { data: assessors, isLoading, error } = useQuery<Assessor[]>({
+    queryKey: [AUTH_KEY, 'assessors'],
+    queryFn: fetchAssessors,
+    retry: false,
+    staleTime: 1000 * 60 * 5
+  });
+
+  return {
+    assessors,
+    isLoading,
+    error
+  };
+}
